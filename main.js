45fddb535d15ad5cc06696ea3f0ee4c11b5ba9f7
Ahmad Rosid Komarudin <ahmadrosid30121997@gmail.com>
2019-04-01 14:45:01 +0700

    Disable Re-Rendering using v-once

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ new Vue({
     el: "#app",
     data: {
         title: 'Hello world!',
-        link: 'https://google.com'
+        link: 'https://google.com',
     },
     methods: {
         changeTitle: function(event) {
