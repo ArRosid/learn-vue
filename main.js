c8b19870364790bd81f7cfa95c9bf030d0d52de6
Ahmad Rosid Komarudin <ahmadrosid30121997@gmail.com>
2019-04-01 14:43:05 +0700

    binding attributes (v-bind)

--- main.js
+++ main.js
@@ -1,7 +1,8 @@
 new Vue({
     el: "#app",
     data: {
-        title: 'Hello world!'
+        title: 'Hello world!',
+        link: 'https://google.com'
     },
     methods: {
         changeTitle: function(event) {
