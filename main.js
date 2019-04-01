c579f90dc64de5d3ee18e4e1037f5ec2d0a7444b
Ahmad Rosid Komarudin <ahmadrosid30121997@gmail.com>
2019-04-01 15:09:05 +0700

    listening to events using v-on:click

--- main.js
+++ main.js
@@ -3,12 +3,17 @@ new Vue({
     data: {
         title: 'Hello world!',
         link: 'https://google.com',
-        link2: '<a href="https://google.com">Google 2</a>'
+        link2: '<a href="https://google.com">Google 2</a>',
+        counter: 0,
     },
     methods: {
         changeTitle: function(event) {
             console.log(event)
             this.title = event.target.value;
+        },
+
+        increase: function() {
+            this.counter++;
         }
     }
 });
