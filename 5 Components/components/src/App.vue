0f5c0f23149303d12ee45cffed21c74d2f2f41a0
Ahmad Rosid Komarudin <ahmadrosid30121997@gmail.com>
2019-04-09 22:24:10 +0700

    start components

--- 5 Components/components/src/App.vue
+++ 5 Components/components/src/App.vue
@@ -1,17 +1,29 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="container">
+    <app-header></app-header>
+    <hr>
+    <div class="row">
+      <servers></servers>
+      <app-server-details></app-server-details>
+    </div>
+    <hr>
+    <app-footer></app-footer>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Header from './components/Header.vue';
+import Footer from './components/Footer.vue';
+import Servers from './components/Servers.vue';
+import ServerDetails from './components/ServerDetails.vue';
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    appHeader: Header,
+    Servers,
+    'app-server-details': ServerDetails,
+    'app-footer': Footer
   }
 }
 </script>
